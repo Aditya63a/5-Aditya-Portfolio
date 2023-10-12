5cc0c00bcd42686cd37abf7d3b0f550ecd30681e
ADITYA  YADAV <129522983+Aditya63a@users.noreply.github.com>
2023-10-12 15:50:07 +0530

    contact form

--- JS/main.js
+++ JS/main.js
@@ -13,8 +13,30 @@ window.onscroll = function () {
 
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(a) {
-    a.addEventListener("click", function(){
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
         navCollapse.classList.remove("show")
     })
+})
+
+// Form Data to Google Sheet
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyEPOKE7nOlK86dubqxA3V2mBAJTvwQgo37RlzlZSKLvNrY9NwoVUYDd0YL-XGwxw_l5Q/exec';
+
+const submit = document.getElementById("submit");
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    submit.innerText = "Submiting...";
+    fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(form)
+    })
+        .then(response => alert("Thank you! for reaching us successfully."))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+
 })
